5dcabb243c83043fe40cd7b90f4cd067547879fd
hnigro <nigroncho@gmail.com>
2024-08-21 18:00:20 -0300

    pc_dtv home hacia cloud_work 19/07/24

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 x = 1000000001
-bw_srt_abc_01 = 11111111
+bw_srt_abc_01 = "20/08/24"
 bw_srt_abc_02 = 22222222
 bw_srt_abc_03 = 33333333
 bw_srt_abc_04 = 44444444
@@ -19,13 +19,16 @@ bw = [bw_srt_abc_01,bw_srt_abc_02,bw_srt_abc_03,bw_srt_abc_04]
 
 @app.route("/")
 def home():
+    xx2 =["q","w","e"]
+
     return render_template("index.html",
                            contenido= f"Testing {x}",
                            x1= f"valor de x1 = {bw_srt_abc_01}",
                            x2= f"valor de x2 = {bw_srt_abc_02}",
                            x3= f"valor de x3 = {bw_srt_abc_03}",
                            x4= f"valor de x4 = {bw_srt_abc_04}",
-                           xx= bw
+                           xx= bw,
+                           xx2 = xx2
                            )
 
 
