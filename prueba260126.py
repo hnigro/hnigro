890b8503c66ae65164afdef798450e7d248b7aee
hnigro <nigroncho@gmail.com>
2026-03-27 15:04:36 -0300

    pc_dtv home hacia cloud_work 19/07/24

--- prueba260126.py
+++ prueba260126.py
@@ -1,14 +1,39 @@
 """
 Este programa es para probar la ui de Fled
 
-1) receptor SRT
-2) analizar archivo SRT
+1) revisar watchdog
+2)
 
 
 
 """
 
+import flet as ft
 
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    input = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        input.value = str(int(input.value) - 1)
+
+    def plus_click(e):
+        input.value = str(int(input.value) + 1)
+
+    page.add(
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                input,
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+            ],
+        )
+    )
+
+ft.run(main)
 
 
 
